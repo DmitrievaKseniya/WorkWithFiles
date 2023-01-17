1f09ed55eadd048279617c1ad3b7b3889e00d88d
DmitrievaKseniya <dkn1933@mail.ru>
2023-01-17 23:19:04 +0400

    Решение к заданию 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
 
 namespace Task1
 {
@@ -6,7 +8,52 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string strPath;
+
+            Console.WriteLine("Введите путь к папке, для очистки:");
+            strPath = Console.ReadLine();
+
+            CleanFolder(strPath);
+            Console.WriteLine("директория успешно очищена от файлов не используемых в течении 30 минут!");
+        }
+
+        static public void CleanFolder(string PathFolder)
+        {
+            TimeSpan interval = TimeSpan.FromMinutes(30);
+
+            if (Directory.Exists(PathFolder))
+            {
+                foreach (string fileInfo in Directory.GetFiles(PathFolder))
+                {
+                    try
+                    {
+                        if ((File.GetLastAccessTime(fileInfo) + interval) < DateTime.Now)
+                            File.Delete(fileInfo);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Не удалось получить доступ к файлу: {fileInfo} по причине: {ex.ToString()}");
+                    }
+                }
+
+                foreach (string dir in Directory.GetDirectories(PathFolder))
+                {
+                    CleanFolder(dir);
+                    try
+                    {
+                        if ((Directory.GetLastAccessTime(dir) + interval) < DateTime.Now)
+                            Directory.Delete(dir);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Не удалось получить доступ к каталогу: {dir} по причине: {ex.ToString()}");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("По указанному пути папки не существует!");
+            }
         }
     }
 }
